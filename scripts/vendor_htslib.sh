593916d4876cfe510801d06b582b7ec42f6cef8a
sounkou-bioinfo <sounkoutoure@gmail.com>
2026-04-20 22:58:29 +0200

    Vendor htslib 1.23.1

--- scripts/vendor_htslib.sh
+++ scripts/vendor_htslib.sh
@@ -5,10 +5,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=vendor_common.sh
 source "$SCRIPT_DIR/vendor_common.sh"
 
-VERSION="1.23"
+VERSION="1.23.1"
 TARBALL="htslib-${VERSION}.tar.bz2"
 URL="https://github.com/samtools/htslib/releases/download/${VERSION}/${TARBALL}"
-SHA256="63927199ef9cea03096345b95d96cb600ae10385248b2ef670b0496c2ab7e4cd"
+SHA256="f8a3f36effeec38f043c53ab1f2d9ed45064f14205c5ef8e3c815763b90803c4"
 
 archive_path="$DIST_DIR/$TARBALL"
 
